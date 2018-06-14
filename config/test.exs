21f3763b7d6f3a9f3837ce71b5af0a39e1d5d180
kamalogudah <mcpaul2058@gmail.com>
2018-06-14 03:21:03 +0300

    Topic Controller

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "kamal",
+  password: "password",
   database: "discuss_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
