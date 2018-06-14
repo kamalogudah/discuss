53a2bd5a379b0ade07b80a6722562f0440965c09
kamalogudah <mcpaul2058@gmail.com>
2018-06-15 02:08:32 +0300

    Setting up an edit form

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,7 @@ defmodule Discuss.Router do
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
     get "/topics", TopicController, :index
+    get "/topics/:id/edit", TopicController, :edit
     
   end
 
