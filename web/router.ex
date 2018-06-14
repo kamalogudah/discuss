8f1207490bf6d80384dc5d3c82b2604f74d34c9b
kamalogudah <mcpaul2058@gmail.com>
2018-06-15 02:38:40 +0300

    Resources Helper

--- web/router.ex
+++ web/router.ex
@@ -16,12 +16,7 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics", TopicController, :index
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+    resources "/", TopicController
 
   end
 
