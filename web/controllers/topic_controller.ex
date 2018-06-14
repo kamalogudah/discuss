fd468d523827585773068e917038234a1ea7467f
kamalogudah <mcpaul2058@gmail.com>
2018-06-14 03:41:51 +0300

    Generating Changesets for the Form

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,11 +1,8 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
+  alias Discuss.Topic
 
   def new(conn,params) do
-    IO.puts "++++++"
-    IO.inspect conn
-    IO.puts "++++++"
-    IO.inspect params
-    IO.puts "++++++"
+    changeset = Topic.changeset(%Topic{},%{})
   end
 end
