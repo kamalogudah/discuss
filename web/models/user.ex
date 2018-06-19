c10b5a3d39fdb817a2682dce894e9c56e9709e41
kamalogudah <mcpaul2058@gmail.com>
2018-06-20 02:40:31 +0300

    Playing around with associations

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,6 +4,7 @@ defmodule Discuss.User do
         field :email, :string
         field :provider, :string
         field :token, :string
+        has_many :topics, Discuss.Topic
 
         timestamps()
     end
