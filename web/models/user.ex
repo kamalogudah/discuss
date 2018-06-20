acad61eedd0f2979bf987051d8ecbe82d359821d
kamalogudah <mcpaul2058@gmail.com>
2018-06-20 11:43:40 +0300

    Comment Model

--- web/models/user.ex
+++ web/models/user.ex
@@ -5,6 +5,7 @@ defmodule Discuss.User do
         field :provider, :string
         field :token, :string
         has_many :topics, Discuss.Topic
+        has_many :comments, Discuss.Comment
 
         timestamps()
     end
