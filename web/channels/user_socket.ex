ff93f3e1286ec356ed701131518eb3230db36659
kamalogudah <mcpaul2058@gmail.com>
2018-07-01 20:57:14 +0300

    Associating Comments with Users

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -11,7 +11,7 @@ defmodule Discuss.UserSocket do
   def connect(%{"token" => token}, socket) do
     case Phoenix.Token.verify(socket, "key", token) do
       {:ok, user_id} ->
-        {:ok, assign(socket,:user_id, user_id) }
+        {:ok, assign(socket, :user_id, user_id)}
 
       {:error, _error} ->
         :error
