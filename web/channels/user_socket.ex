f11f9527180bcb86a74df85ea69ecd0d31a20422
kamalogudah <mcpaul2058@gmail.com>
2018-07-01 18:34:06 +0300

    Verifying current user token

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -9,8 +9,13 @@ defmodule Discuss.UserSocket do
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   def connect(%{"token" => token}, socket) do
-    IO.puts token
-    {:ok, socket}
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket,:user_id, user_id) }
+
+      {:error, _error} ->
+        :error
+    end
   end
 
   def id(_socket), do: nil
