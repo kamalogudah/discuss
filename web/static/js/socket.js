0c8ae57c69256cbbc63414148dfd5867c88e71d3
kamalogudah <mcpaul2058@gmail.com>
2018-07-01 17:33:17 +0300

    Rendering a list of comments

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -6,13 +6,31 @@ socket.connect()
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive('ok', resp => {
+       renderComments(resp.comments);
+
+     })
+    .receive('error', resp => {
+      console.log('Unable to join', resp);
+     });
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
 
     channel.push('comment:add',  { content: content });
   });
+};
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </l>
+    `;
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+
 }
 
 window.createSocket = createSocket;
