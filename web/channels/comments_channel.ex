bc650b3baca5edc3f5de41df9e16657e4a7b5299
kamalogudah <mcpaul2058@gmail.com>
2018-07-01 16:59:22 +0300

    Building the comment form for real

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,15 +1,27 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
   alias Discuss.Topic
+  alias Discuss.Comment
 
   def join("comments:" <> topic_id,_params,socket) do
     topic_id = String.to_integer(topic_id)
     topic = Repo.get(Topic, topic_id)
 
-    {:ok, %{}, socket}
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
   def handle_in(name, %{"content" => content}, socket) do
-    {:reply, :ok, socket }
+    topic = socket.assigns.topic
+
+    changeset = topic
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket }
+      {:error,_reason} ->
+         {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
